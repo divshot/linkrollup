387927d545ae0c471065c934fdf99e476c3edfaf
Jake Johnson <jake@fastdivision.com>
2012-11-05 12:13:47 -0800

    Fixed error when displaying buffered tweets without links

--- app.rb
+++ app.rb
@@ -7,9 +7,14 @@ config_file 'config/env.yml'
 
 helpers do
   def updateLink(updateText)
-    linkUrl = updateText.scan(/(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/)[0][0]
-    link = updateText.sub(linkUrl, '').gsub(/(?:^|\s)(#\S+)/, '').strip
-    "<li><a href='#{linkUrl}'>#{link}</a></li>"
+    linkUrl = updateText.scan(/(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/)
+    unless linkUrl.empty?
+      link = updateText.sub(linkUrl[0][0], '').gsub(/(?:^|\s)(#\S+)/, '').strip
+      "<li><a href='#{linkUrl[0][0]}'>#{link}</a></li>"
+    else
+      link = updateText.gsub(/(?:^|\s)(#\S+)/, '').strip
+      "<li>#{link}</li>"
+    end
   end
 end
 
